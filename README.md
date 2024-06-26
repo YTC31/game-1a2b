17ebeba83b3e0961a5996e13ad2ccc19239bb7f0
Alena Chang <alenachang12@gmail.com>
2024-06-26 23:55:09 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# RWD Calendar Practice using React and Tailwind
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+  - [Built with](#built-with)
+
+## Overview
+
+### Links
+- Live Site URL: [TBC](https://)
+
+### Built with
+
+- HTML
+- JavaScript
+- [Bootstrap](https://getbootstrap.com/)
---
+++ main.js
@@ -0,0 +1,128 @@
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
+
+let answer, countA, countB;
+const btnStart = document.querySelector('.btn-group :nth-child(1)')
+const btnRestart = document.querySelector('.btn-group :nth-child(2)')
+const btnShowAnswer = document.querySelector('.btn-group :nth-child(3)')
+const btnGuess = document.getElementById('btnGuess')
+const inputGuessBox = document.querySelector('.form-control')
+const listGameHistory = document.getElementById('gameHistory')
+const answerModal = document.getElementById('answerModal')
+const inputModal = document.getElementById('inputModal')
+
+
+btnStart.addEventListener('click', initGame)
+btnRestart.addEventListener('click', initGame)
+btnShowAnswer.addEventListener('click', showAnswer)
+btnGuess.addEventListener('click', checkAnswer)
+inputGuessBox.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      checkAnswer();
+    }
+  });
+
+
+function initGame() {
+    listGameHistory.innerHTML = ""
+    generateAnswer()
+    changeBtnStatus()
+}
+
+function generateAnswer() {
+    answer = []
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    while (answer.length < 4) {
+        let number = numbers[Math.floor(Math.random() * numbers.length)]
+        let index = numbers.indexOf(number)
+        answer.push(number)
+        numbers.splice(index, 1)
+    }
+}
+
+function changeBtnStatus() {
+    btnRestart.disabled = false
+    btnShowAnswer.disabled = false
+    btnGuess.disabled = false
+    inputGuessBox.disabled = false
+    inputGuessBox.placeholder = "請輸入 4 個不重複數字"
+    btnStart.disabled = true
+}
+
+function showAnswer() {
+    answerModal.addEventListener('shown.bs.modal', () => {
+        document.querySelector('#answerModal .modal-body').innerHTML = `<p><strong>${answer.join('')}<strong></p>`
+    })
+}
+
+function checkAnswer() {
+    const guessValue = inputGuessBox.value.trim()
+    const digitSet = new Set(guessValue)
+    const regExpforNum = /^[0-9]{4}$/
+    if (!regExpforNum.test(guessValue) || digitSet.size !== 4) {
+        alert('請輸入符合規定的數字（4 個不重複數字，不含任何符號)')
+        clearInputBox()
+        return
+    } else {
+        compareAnswer(guessValue)
+        renderAnswer(guessValue)
+    }
+}
+
+function compareAnswer(guessValue) {
+    const strAnswer = answer.join('')
+    countA = 0
+    countB = 0
+    for (let i = 0; i < strAnswer.length; i++) {
+        if (strAnswer[i] == guessValue[i]) {
+            countA++
+        } else if (strAnswer.includes(guessValue[i])) {
+            countB++
+        }
+    }
+    if (countA === 4) {
+        alert(`恭喜猜對！答案就是 ${answer.join('')}`)
+    }
+}
+
+function renderAnswer(guessValue) {
+    const li = document.createElement('li')
+    li.setAttribute('class', 'list-group-item')
+    let color = countA === 4 ? "bg-danger" : "bg-success"
+    li.innerHTML = `第 ${checkGuessTimes()} 次猜：<span class="badge ${color}">${countA}A${countB}B</span> ${guessValue} `
+    listGameHistory.append(li)
+    clearInputBox()
+}
+
+function clearInputBox() {
+    inputGuessBox.value = ''
+}
+
+function checkGuessTimes() {
+    let liCounts = document.getElementsByTagName('li').length
+    return liCounts + 1
+}
+
+
